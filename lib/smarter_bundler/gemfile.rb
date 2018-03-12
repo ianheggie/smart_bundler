b12902d203a8012a4fb7f74a98edca197c7bc7bc
Ian Heggie <ian@heggie.biz>
2018-03-13 00:44:53 +1100

    Fix test for blank version

--- lib/smarter_bundler/gemfile.rb
+++ lib/smarter_bundler/gemfile.rb
@@ -15,7 +15,7 @@ module SmarterBundler
     end
 
     def restrict_gem_version gem, version_limit
-      return false if version_limit.blank?
+      return false unless version_limit.to_s =~ /\d\.\d/
       if @contents.select{|line| line =~ /^\s*gem\s+['"]#{gem}['"]/}.empty?
         @contents << "gem '#{gem}', '>=0'"
       end
